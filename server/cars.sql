fc1e987e4d3c7d7a475cf53c6b6f298a3026c676
Puggen <112873174+Puggen@users.noreply.github.com>
2024-01-02 14:56:04 +0100

    change

--- server/cars.sql
+++ server/cars.sql
@@ -13,9 +13,9 @@ INSERT INTO cars(id,model,manufac,regnr,color) VALUES (2,'R8','Audi','MIT 782','
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (3,'520i','BMW','LKI 099','purple');
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (4,'9000','SAAB','LKO 081','gray');
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (5,'Polo','Volkswagen','JBV 412','gray');
-INSERT INTO cars(id,model,manufac,regnr,color) VALUES (6,'impreza','Subaru','DTY 621','yello');
+INSERT INTO cars(id,model,manufac,regnr,color) VALUES (6,'impreza','Subaru','DTY 621','yellow');
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (7,'Uno','Fiat','FPM 611','yellow');
-INSERT INTO cars(id,model,manufac,regnr,color) VALUES (8,'320i','BMW','MBC 831','gregnren');
+INSERT INTO cars(id,model,manufac,regnr,color) VALUES (8,'320i','BMW','MBC 831','green');
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (9,'900','SAAB','OPI 977','yellow');
 INSERT INTO cars(id,model,manufac,regnr,color) VALUES (10,'Golf','Volkswagen','MLB 373','yellow');
 
