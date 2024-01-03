a229ced0156a438399d690f3209ed3f167d85b01
Puggen <112873174+Puggen@users.noreply.github.com>
2024-01-03 13:26:27 +0100

    change

--- server/cars.sql
+++ server/cars.sql
@@ -4,20 +4,21 @@ CREATE TABLE IF NOT EXISTS  cars(
   ,model    VARCHAR(8) NOT NULL
   ,manufac  VARCHAR(9) NOT NULL
   ,regnr    VARCHAR(16) NOT NULL
-  ,fuel    VARCHAR(6) NOT NULL
+  ,color    VARCHAR(6) NOT NULL
+  ,fuel     VARCHAR(10) NOT NULL
 );
   
 
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (1,'740','Volvo','HGT 435','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (2,'R8','Audi','MIT 782','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (3,'520d','BMW','LKI 099','disel');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (4,'9000','SAAB','LKO 081','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (5,'Polo','Volkswagen','JBV 412','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (6,'impreza','Subaru','DTY 621','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (7,'Uno','Fiat','FPM 611','electric');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (8,'Model Y','Tesla','MBC 831','electric');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (9,'900','SAAB','OPI 977','petrol');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (10,'Golf','Volkswagen','MLB 373','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (1,'740','Volvo','HGT 435','red','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (2,'R8','Audi','MIT 782','gray','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (3,'520d','BMW','LKI 099','purple','disel');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (4,'9000','SAAB','LKO 081','gray','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (5,'golf','Volkswagen','JBV 412','gray','el');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (6,'impreza','Subaru','DTY 621','yellow','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (7,'Uno','Fiat','FPM 611','yellow','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (8,'Model X','Tesla','MBC 831','white','el');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (9,'900','SAAB','OPI 977','yellow','petrol');
+INSERT INTO cars(id,model,manufac,regnr,color,fuel) VALUES (10,'Golf','Volkswagen','MLB 373','yellow','petrol');
 
 
 select * from cars;
