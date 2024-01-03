55db12674eeb0700a847bf6a9bd01855818602c8
Puggen <112873174+Puggen@users.noreply.github.com>
2024-01-03 11:59:06 +0100

    change

--- server/cars.sql
+++ server/cars.sql
@@ -8,16 +8,16 @@ CREATE TABLE IF NOT EXISTS  cars(
 );
   
 
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (1,'740','Volvo','HGT 435','red');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (2,'R8','Audi','MIT 782','gray');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (3,'520i','BMW','LKI 099','purple');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (4,'9000','SAAB','LKO 081','gray');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (5,'Polo','Volkswagen','JBV 412','gray');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (6,'impreza','Subaru','DTY 621','yellow');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (7,'Uno','Fiat','FPM 611','yellow');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (8,'320i','BMW','MBC 831','green');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (9,'900','SAAB','OPI 977','yellow');
-INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (10,'Golf','Volkswagen','MLB 373','yellow');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (1,'740','Volvo','HGT 435','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (2,'R8','Audi','MIT 782','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (3,'520d','BMW','LKI 099','disel');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (4,'9000','SAAB','LKO 081','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (5,'Polo','Volkswagen','JBV 412','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (6,'impreza','Subaru','DTY 621','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (7,'Uno','Fiat','FPM 611','electric');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (8,'Model Y','Tesla','MBC 831','electric');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (9,'900','SAAB','OPI 977','petrol');
+INSERT INTO cars(id,model,manufac,regnr,fuel) VALUES (10,'Golf','Volkswagen','MLB 373','petrol');
 
 
 select * from cars;
